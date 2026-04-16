bd0193e0afe17b37d2ae8d354c1bc1f964bf706e
Pushpalatha58 <pushpalathanv12@gmail.com>
2026-04-16 15:21:23 +0530

    Add files via upload

--- glue_drift/__init__.py
+++ glue_drift/__init__.py
@@ -1,1 +1,19 @@
-keep
+"""
+glue-drift
+----------
+Detect configuration drift in AWS Glue jobs.
+Compare live job configs against a source-of-truth YAML.
+"""
+
+__version__ = "0.1.0"
+__author__ = "Pushpalatha58"
+
+from glue_drift.checker import check_all_jobs, JobDriftResult
+from glue_drift.reporter import print_terminal_report, write_json_report
+
+__all__ = [
+    "check_all_jobs",
+    "JobDriftResult",
+    "print_terminal_report",
+    "write_json_report",
+]
